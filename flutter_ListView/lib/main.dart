ecd2fad90eec3021e9b9b2ca2ba655bf7b0afe99
张凯 <zhangkai@RicharddeMacbook-pro.local>
2019-08-15 18:03:49 +0800

    横向ListView组件嵌套

--- flutter_ListView/lib/main.dart
+++ flutter_ListView/lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp(
-    // arr: List(), // 不固定长度的 List，相当于数组
-    items: new List<String>.generate(1000, (i) => "Item $i")
-));
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-    // 声明一个常量为 List 类型
-    final List<String> items;
-    // @required 表示必须传入一个参数
-    MyApp({Key key, @required this.items}):super(key: key);
-
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
@@ -19,16 +11,42 @@ class MyApp extends StatelessWidget {
                 appBar: new AppBar(
                     title: new Text('Hello Flutter'),
                 ),
-                body: new ListView.builder( // new 一个动态加载的 ListView
-                    itemCount: items.length, // 需要生产多少条数据、
-                    itemBuilder: (context, index) {
-                        return new ListTile(
-                            title: new Text('${items[index]}'),
-                        );
-                    },
-                ),
+                body: Center(
+                    child: Container(
+                        height: 200.0,
+                        child: MyList(),
+                    ),
+                )
             ),
         );
     }
 }
 
+class MyList extends StatelessWidget{
+    @override
+    Widget build(BuildContext context){
+        return ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+                new Container(
+                    width: 100.0,
+                    height: 200.0,
+                    color: Colors.greenAccent,
+                    child: new Text(
+                        'zhangkai',
+                    ),
+                ),
+                new Container(
+                    width: 100.0,
+                    height: 200.0,
+                    color: Colors.yellow,
+                ),
+                new Container(
+                    width: 100.0,
+                    height: 200.0,
+                    color: Colors.deepOrange,
+                ),
+            ],
+        );
+    }
+}
